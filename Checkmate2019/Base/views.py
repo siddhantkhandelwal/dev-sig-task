280cfe8be0667e5754e3450bd888ec62590e0d4d
dush-t <45326332+dush-t@users.noreply.github.com>
2019-01-08 18:29:46 +0530

    [Update] BugFix: Handling invalid BITS IDs better
    Now an error message will be displayed when a user enters an invalid BITS ID.

--- Checkmate2019/Base/views.py
+++ Checkmate2019/Base/views.py
@@ -15,7 +15,7 @@ def index(request):
         return render(request, "Base/index.html", {})
     return render(request, "Base/index.html", {})
 
-
+    
 def sign_up(request):
     if request.method == 'POST':
         form = Sign_up(request.POST)
@@ -45,6 +45,9 @@ def sign_up(request):
             form = Sign_up()
             messages.error(request, 'Please enter valid BITS IDs')
             return render(request, 'Base/sign_up.html', {'form': form})
+    else:
+            form = Sign_up()
+            return render(request, 'Base/sign_up.html', {'form': form})    
 
 
 def sign_in(request):
